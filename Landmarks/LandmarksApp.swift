36cc9b49bc2b8434488ce4516b459aac7f9e1204
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2023-07-10 14:43:02 +0900

    ObservableObjectを追加してお気に入りを変更した時に監視できるようにした

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,12 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by user on 2023/07/10.
-//
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
